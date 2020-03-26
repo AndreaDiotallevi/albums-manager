4759270acb3fbcf8f700d39749ba0a1bd95dc66d
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-26 19:38:34 +0000

    Delete comment out code in album test file

--- client/src/components/Album/Album.test.js
+++ client/src/components/Album/Album.test.js
@@ -10,23 +10,6 @@ const setUp = (Component, props = {}) => {
 };
 
 describe('Album', () => {
-  // it('should render the title YOUR ALBUMS', () => {
-  //   const wrapper = setUp(Album, { albums: [] });
-  //   const h1 = wrapper.find('[data-test='albums-page-title']');
-  //   expect(h1.text()).toEqual('YOUR ALBUMS');
-  // });
-
-  // it('should render the album title and artist', () => {
-  //   const props = { albums: [{ artist: 'Tycho', title: 'Awake' }] };
-  //   const wrapper = setUp(Album, props);
-
-  //   const h2 = wrapper.find('[data-test='album-title']');
-  //   const h3 = wrapper.find('[data-test='album-artist']');
-
-  //   expect(h2.text()).toEqual('Awake');
-  //   expect(h3.text()).toEqual('Tycho');
-  // })
-
   it('should fetch the album info', async () => {
     const albumData = {
       album: {
