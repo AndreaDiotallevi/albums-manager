15d845ebbc24fac79e9f10484e5725bb79f7b9df
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-26 11:55:09 +0000

    Change home page title

--- server.js
+++ server.js
@@ -7,21 +7,17 @@ const app = express();
 app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const url = require('./config/keys').mongoURI;
 
 // Connect to Mongo
 if (process.env.NODE_ENV !== 'test') {
   mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+    .connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 }
 
 // Use Routes
 app.use('/api/albums', albums);
-
-// const port = process.env.PORT || 5000;
-
-// app.listen(port, () => console.log(`Server started on port ${port}`));
 
 module.exports = app
