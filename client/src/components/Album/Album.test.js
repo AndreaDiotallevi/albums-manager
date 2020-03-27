afa7458e99b62a74c0fa58b21e56a49cc3b40ea1
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-27 12:51:55 +0000

    Add test for AlbumInput component axios call

--- client/src/components/Album/Album.test.js
+++ client/src/components/Album/Album.test.js
@@ -2,26 +2,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Album from './Album';
 import mockAxios from 'axios';
-const lastFmApiKey = require('../../config/keys').lastFmApiKey;
 
 describe('Album', () => {
   it('should fetch the album info', async () => {
-    const albumData = {
-      album: {
-        artist: 'Tycho',
-        name: 'Awake',
-        image: [null, null, null, null, { '#text': 'url' }],
-        tracks: {
-          track: [{ name: 'Awake' }, { name: 'Montana' }]
-        }
-      }
+    const album = {
+      _id: 0,
+      artist: 'Tycho',
+      title: 'Awake',
+      posterURL: 'url',
+      tracks: ['Awake', 'Montana']
     }
 
     mockAxios.get.mockImplementationOnce(() => Promise.resolve({
-      data: albumData
+      data: album
     }));
 
-    const props = { match: { params: { artist: 'Tycho', title: 'Awake' } } };
+    const props = { match: { params: { id: '0' } } };
 
     const wrapper = await shallow(<Album {...props}/>);
 
@@ -33,6 +29,6 @@ describe('Album', () => {
     });
 
     expect(mockAxios.get).toHaveBeenCalledTimes(1);
-    expect(mockAxios.get).toHaveBeenCalledWith(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${lastFmApiKey}&artist=Tycho&album=Awake&format=json`);
+    expect(mockAxios.get).toHaveBeenCalledWith('/api/albums/0');
   });
 });
