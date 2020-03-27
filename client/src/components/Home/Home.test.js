974484b33a751d8c0a6d5a84faefdd9453560a42
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-27 16:21:37 +0000

    Amend tests for the Home component

--- client/src/components/Home/Home.test.js
+++ client/src/components/Home/Home.test.js
@@ -2,15 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Home from './Home';
 
-const setUp = (Component, props = {}) => {
-  const component = shallow(<Component {...props} />);
-  return component;
-};
-
 describe('Home', () => {
   it("should render the title ALBUMS MANAGER", () => {
-    const wrapper = setUp(Home, {});
-    const h1 = wrapper.find("[data-test='home-page-title']");
-    expect(h1.text()).toEqual('ALBUMS MANAGER');
+    const wrapper = shallow(<Home />);
+    expect(wrapper.find("[data-test='home-page-title']").text()).toEqual('ALBUMS MANAGER');
   });
 });
