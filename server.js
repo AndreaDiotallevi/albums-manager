e2f766e6f1bd270f4c84c3892483c779e7eef8cc
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-30 19:53:54 +0100

    Deploy to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const albums = require('./routes/api/albums')
 
@@ -19,5 +20,14 @@ if (process.env.NODE_ENV !== 'test') {
 
 // Use Routes
 app.use('/api/albums', albums);
+
+// Setup for Production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 module.exports = app
